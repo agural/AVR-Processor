138ce6a3a6d890b678221fb234c69ce7c63e1926
Peter Cuy <pjcuy1@gmail.com>
2015-01-25 18:00:39 -0800

    Updated REG_TEST_TB

--- src/AVR/REG_TEST_TB.vhd
+++ src/AVR/REG_TEST_TB.vhd
@@ -1,46 +1,80 @@
--- TestBench Template 
+----------------------------------------------------------------------------
+--
+--  Atmel AVR Register Array Test Bench
+--
+--  Revision History:
+--      01/25/15    Peter Cuy       initial version
+--
+----------------------------------------------------------------------------
+-- bring in the necessary packages
+library ieee;
+use ieee.std_logic_1164.all;
 
-  LIBRARY ieee;
-  USE ieee.std_logic_1164.ALL;
-  USE ieee.numeric_std.ALL;
-
-  ENTITY testbench IS
-  END testbench;
-
-  ARCHITECTURE behavior OF testbench IS 
-
-  -- Component Declaration
-          COMPONENT <component name>
-          PORT(
-                  <port1> : IN std_logic;
-                  <port2> : IN std_logic_vector(3 downto 0);       
-                  <port3> : OUT std_logic_vector(3 downto 0)
-                  );
-          END COMPONENT;
-
-          SIGNAL <signal1> :  std_logic;
-          SIGNAL <signal2> :  std_logic_vector(3 downto 0);
-          
-
-  BEGIN
-
-  -- Component Instantiation
-          uut: <component name> PORT MAP(
-                  <port1> => <signal1>,
-                  <port3> => <signal2>
-          );
+library opcodes;
+use opcodes.opcodes.all;
 
 
-  --  Test Bench Statements
-     tb : PROCESS
-     BEGIN
+entity REG_TEST_TB is
+end REG_TEST_TB;
 
-        wait for 100 ns; -- wait until global set/reset completes
+architecture TB_ARCHITECTURE of REG_TEST_TB is 
+    component REG_TEST
+        port (
+            IR       :  in  opcode_word;                        -- Instruction Register
+            RegIn    :  in  std_logic_vector(7 downto 0);       -- input register bus
+            clock    :  in  std_logic;                          -- system clock
+            RegAOut  :  out std_logic_vector(7 downto 0);       -- register bus A out
+            RegBOut  :  out std_logic_vector(7 downto 0)  
+        );
+    end component;
+    
+    signal IR           :   opcode_word;                        -- Instruction Register
+    signal RegIn        :   std_logic_vector(7 downto 0);       -- input register bus
+    signal clock        :   std_logic;                          -- system clock
+    signal RegAOut      :   std_logic_vector(7 downto 0);       -- register bus A out
+    signal RegBOut      :   std_logic_vector(7 downto 0);  
+    signal  END_SIM     :  BOOLEAN := FALSE;                   -- end simulation flag
+    
+    begin
+    UUT : REG_TEST
+        port map(
+            IR      =>  IR     , 
+            RegIn   =>  RegIn  , 
+            clock   =>  clock  , 
+            RegAOut =>  RegAOut, 
+            RegBOut =>  RegBOut 
+        );
+        
+    process
+    begin
+    
+        END_SIM <= TRUE;    --end of stimulus events
+        wait;               --wait for the simulation to end
+    end process;
+    
+    CLOCK_CLK : process
+  
+    begin
 
-        -- Add user defined stimulus here
+        -- this process generates a 20 ns period, 50% duty cycle clock
 
-        wait; -- will wait forever
-     END PROCESS tb;
-  --  End Test Bench 
+        -- only generate clock if still simulating
 
-  END;
+        if END_SIM = FALSE then
+            CLK <= '0';
+            wait for 10 ns;
+        else
+            wait;
+        end if;
+
+        if END_SIM = FALSE then
+            CLK <= '1';
+            wait for 10 ns;
+        else
+            wait;
+        end if;
+
+    end process;
+
+end TB_ARCHITECTURE;
+
