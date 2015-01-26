565fec7bb8b95bb264d7eecff1f525ea4d348557
Peter Cuy <pjcuy1@gmail.com>
2015-01-26 15:59:16 -0800

    Updated Reg_TEST

--- src/AVR/REG_TEST_TB.vhd
+++ src/AVR/REG_TEST_TB.vhd
@@ -21,54 +21,12 @@ entity REG_TEST_TB is
     constant NUM_REGS : integer := 32;
     type REG_ARRAY is array (0 to NUM_REGS-1) of std_logic_vector(7 downto 0);
 
-function CALC_FLAGS (flags          : std_logic_vector(7 downto 0);
-                     result         : std_logic_vector(7 downto 0);
-                     carry          : std_logic;
-                     overflow       : std_logic;
-                     halfcarry      : std_logic;
-                     transferbit    : std_Logic;
-                     flagmask       : std_logic_vector(7 downto 0);
-                     bypassFlagCalc : std_logic)
-                     return std_logic_vector is
-     variable newFlags : std_logic_vector(7 downto 0) := "00000000";
-begin
-    if (bypassFlagCalc = '0') then
-        newFlags(flag_C) := carry;
-        if (result = "00000000") then 
-            newFlags(flag_Z) := '1';
-        end if;    
-        newFlags(flag_N) := result(7);
-        newFlags(flag_V) := overflow;
-        newFlags(flag_S) := result(7) xor overflow;
-        newFlags(flag_H) := halfcarry;
-        newFlags(flag_T) := transferbit;
-    else
-        newFlags := result;
-    end if;
-        newFlags := (newFlags and flagmask) or (flags and (not flagmask));
-    return result;
-end CALC_FLAGS;
-
-function CALC_CARRY (A  : std_logic_vector(7 downto 0);
-                         B  : std_logic_vector(7 downto 0);
-                         carryin: std_logic;
-                         carrybit: integer) --calculate carry out of carrybit
-                         return std_logic is
-     variable temp : std_logic_vector( (carrybit+1) downto 0);
-begin
-    temp := std_logic_vector( unsigned('0' & A(carrybit downto 0)) + 
-                              unsigned('0' & B(carrybit downto 0)) +
-                              unsigned(std_logic_vector(to_unsigned(0,carrybit+1)) & carryin) );
-    return temp(carrybit+1);
-end CALC_CARRY;
-
 procedure ADC_OP 
     (variable OP1        : in    integer;
      variable OP2        : in    integer;
      signal Registers  : inout REG_ARRAY;
-     signal flags      : inout std_logic_vector(7 downto 0);
      signal IR         : out   opcode_word;
-     signal NextRegIn  : out   std_logic_vector(7 downto 0);
+     signal NextRegIn  : in   std_logic_vector(7 downto 0);
      signal ExpRegAOut : out   std_logic_vector(7 downto 0);
      signal ExpRegBOut : out   std_logic_vector(7 downto 0)) is
      
@@ -83,30 +41,8 @@ begin
     --set expected register A and B out
     ExpRegAOut <= Registers(OP1);
     ExpRegBOut <= Registers(OP2);
-    result:= std_logic_vector(unsigned(Registers(OP1)) + unsigned(Registers(OP2)) + unsigned(std_logic_vector(to_unsigned(0,7)) & flags(flag_C)));
-    NextRegIn <= result;
-    flags <= CALC_FLAGS (flags          =>   flags  ,
-                         result         =>  result,
-                         carry          => CALC_CARRY(Registers(OP1), Registers(OP2), flags(flag_C), 7),
-                         
-                         overflow       => (CALC_CARRY(Registers(OP1), Registers(OP2), flags(flag_C), 7) xor
-                                           CALC_CARRY(Registers(OP1), Registers(OP2), flags(flag_C), 6)),
-                                           
-                         halfcarry      => CALC_CARRY(Registers(OP1), Registers(OP2), flags(flag_C), 3),
-                         transferbit    => '0',
-                         flagmask       => flag_mask_ZCNVSH,
-                         bypassFlagCalc => '0'
-    );
-
-    
+    Registers(OP1) <= NextRegIn;
 end ADC_OP;
-
-
-                         
-
-
-
-
 
 end REG_TEST_TB;
 
@@ -134,8 +70,7 @@ architecture TB_ARCHITECTURE of REG_TEST_TB is
  
     signal ExpRegAOut      :   std_logic_vector(7 downto 0);    -- expected register bus A out
     signal ExpRegBOut      :   std_logic_vector(7 downto 0);    -- expected register bus B out
-    signal flags           :   std_logic_vector(7 downto 0);
-
+    signal NextRegIn       :   std_logic_vector(7 downto 0);
     signal  END_SIM     :  BOOLEAN := FALSE;                   -- end simulation flag
 
     
@@ -160,7 +95,9 @@ architecture TB_ARCHITECTURE of REG_TEST_TB is
         OP1 := 0;
         OP2 := 31;
         
-        ADC_OP(OP1,OP2,Registers,flags, IR, RegIn, ExpRegAOut, ExpRegBOut);
+        NextRegIn <= "00000000";
+        ADC_OP(OP1,OP2,Registers, IR, NextRegIn, ExpRegAOut, ExpRegBOut);
+        
         wait for 20 ns;
         wait for 20 ns;
         
