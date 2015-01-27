2a9007537b968bbd7a81a50568facbaa3d01a455
Peter Cuy <pjcuy1@gmail.com>
2015-01-26 20:56:16 -0800

    Added ALU constants, started AVRControl

--- src/AVR/ALUCommands.vhd
+++ src/AVR/ALUCommands.vhd
@@ -19,7 +19,45 @@ use ieee.std_logic_1164.all;
 
 package ALUCommands is
 
-
+--  ALU Blocks for ALUBlockSel
+    constant ALUAddBlock    : std_logic_vector(1 downto 0) := "00";
+    constant ALUFBlock      : std_logic_vector(1 downto 0) := "01";
+    constant ALUShiftBlock  : std_logic_vector(1 downto 0) := "10";
+    constant ALUMulBlock    : std_logic_vector(1 downto 0) := "11";
+    
+--  ALU Block Instructions for BlockInstructionSel
+    --F Block
+    constant FBlockZERO         : std_logic_vector(3 downto 0) := "0000";
+    constant FBlockNOR          : std_logic_vector(3 downto 0) := "0001";
+    constant FBlockNOTA         : std_logic_vector(3 downto 0) := "0011";
+    constant FBlockNOTB         : std_logic_vector(3 downto 0) := "0101";
+    constant FBlockXOR          : std_logic_vector(3 downto 0) := "0110";
+    constant FBlockNAND         : std_logic_vector(3 downto 0) := "0111";
+    constant FBlockAND          : std_logic_vector(3 downto 0) := "1000";
+    constant FBlockXNOR         : std_logic_vector(3 downto 0) := "1001";
+    constant FBlockOR           : std_logic_vector(3 downto 0) := "1110";
+    constant FBlockONE          : std_logic_vector(3 downto 0) := "1111";
+    
+    --Shift Block
+    --bit 2 - shift 1 vs shift 4
+    --bit 1:0 - high bit source: 00 => 0, 01 => A0, 10 => An
+    constant ShiftBlockArith    : std_logic_vector(3 downto 0) := "0010";
+    constant ShiftBlockLog      : std_logic_vector(3 downto 0) := "0000";
+    constant ShiftBlockRot      : std_logic_vector(3 downto 0) := "0001";
+    constant ShiftBlockSwap     : std_logic_vector(3 downto 0) := "0100";
+    
+    --Add Block
+    constant AddBlockAdd        : std_logic_vector(3 downto 0) := "0000";
+    constant AddBlockSub        : std_logic_vector(3 downto 0) := "0010";
+    constant AddBlockAddCarr    : std_logic_vector(3 downto 0) := "0001";
+    constant AddBlockSubCarr    : std_logic_vector(3 downto 0) := "0011";
+    constant AddBlockNeg        : std_logic_vector(3 downto 0) := "0100";
+    
+    --Multiply Block
+    constant MulBlockLowByte    : std_logic_vector(3 downto 0) := "0000";
+    constant MulBlockHighByt    : std_logic_vector(3 downto 0) := "0001";
+    
+    
 --  ALU Flags
     constant flag_C     :   integer := 0;
     constant flag_Z     :   integer := 1;
@@ -36,8 +74,12 @@ package ALUCommands is
     constant flag_mask_ZNVS   : std_logic_vector(7 downto 0) := "00011110";
     constant flag_mask_T      : std_logic_vector(7 downto 0) := "01000000";
     constant flag_mask_ZC     : std_logic_vector(7 downto 0) := "00000011";
---  ALU commands
+    constant flag_mask_KEEPALL: std_logic_vector(7 downto 0) := "00000000";
+    
 
+--  ALU Operand 2 Select (ALUOp2Sel) Values
+    constant RegOp2         : std_logic := '0';     --Operand 2 is from registers
+    constant ImmedOp2       : std_logic := '1';     --Operand 2 is immediate
 
 
 end package;
--- src/AVR/AVRControl.vhd
+++ src/AVR/AVRControl.vhd
@@ -1,41 +1,187 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    15:43:50 01/25/2015 
--- Design Name: 
--- Module Name:    AVRControl - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
+----------------------------------------------------------------------------
+--  AVR Processor Control Block
+--  Revision History:
+--      01/26/15        Peter Cuy       initial version
 --
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+----------------------------------------------------------------------------
+-- bring in the necessary packages
+library  ieee;
+use  ieee.std_logic_1164.all;
+use ieee.numeric_std.all; 
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+library opcodes;
+use opcodes.opcodes.all;
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+library ALUCommands;
+use ALUCommands.ALUCommands.all;
+
 
 entity AVRControl is
+    port (
+            clock                   :  in  std_logic;                          --the system clock
+            IR                      :  in  opcode_word;                        -- Instruction Register
+            ALUStatusMask           :  out  std_logic_vector(7 downto 0);
+            ALUBitChangeEn          :  out  std_logic;
+            ALUBitClrSet            :  out  std_logic;
+            ALUBitTOp               :  out  std_logic;
+            ALUOp2Sel               :  out  std_logic;
+            ImmediateOut            :  out  std_logic_vector(7 downto 0);
+            ALUBlockSel             :  out  std_logic_vector(1 downto 0);
+            ALUBlockInstructionSel  :  out  std_logic_vector(3 downto 0);
+            
+            --Outputs to registers
+            EnableIn    :   out  std_logic;
+            SelIn       :   out  std_logic_vector(5 downto 0);
+            SelA        :   out  std_logic_vector(5 downto 0);
+            SelB        :   out  std_logic_vector(5 downto 0)
+        );
 end AVRControl;
 
 architecture DataFlow of AVRControl is
-
+    signal CycleCount  :   std_logic := '0';
 begin
 
+    DecodeInstruction: process (IR)
+    begin
+        ALUOp2Sel <= RegOp2;        --default operand 2 is from register
+        if std_match(IR, OpADC   ) then 
+            ALUStatusMask <= flag_mask_ZCNVSH;
+            ALUBlockSel <= ALUAddBlock;
+        end if;
 
+        if std_match(IR, OpADD   ) then 
+            ALUStatusMask <= flag_mask_ZCNVSH;
+            ALUBlockSel <= ALUAddBlock;
+        end if;
+        
+        if std_match(IR, OpADIW  ) then 
+            ALUStatusMask <= flag_mask_ZCNVS;
+            ALUOp2Sel<= ImmedOp2;
+            ALUBlockSel <= ALUAddBlock;
+        end if;
+        
+        if std_match(IR, OpAND   ) then 
+            ALUStatusMask <= flag_mask_ZNVS;
+            ALUBlockSel <= ALUFBlock;
+        end if;
+        
+        if std_match(IR, OpANDI  ) then 
+            ALUOp2Sel<= ImmedOp2;
+            ALUStatusMask <= flag_mask_ZNVS;
+            ALUBlockSel <= ALUFBlock;
+        end if;
+        
+        if std_match(IR, OpASR   ) then 
+            ALUStatusMask <= flag_mask_ZCNVS;
+            ALUBlockSel <= ALUShiftBlock;
+        end if;
+        
+        if std_match(IR, OpBCLR  ) then 
+        end if;
+        
+        if std_match(IR, OpBLD   ) then 
+        end if;
+        
+        if std_match(IR, OpBSET  ) then 
+        end if;
+        
+        if std_match(IR, OpBST   ) then 
+        
+        end if;
+        
+        if std_match(IR, OpCOM   ) then 
+            ALUStatusMask <= flag_mask_ZCNVS;
+            ALUBlockSel <= ALUFBlock;
+        end if;
+        
+        if std_match(IR, OpCP    ) then 
+            ALUStatusMask <= flag_mask_ZCNVSH;
+        end if;
+        
+        if std_match(IR, OpCPC   ) then 
+            ALUStatusMask <= flag_mask_ZCNVSH;
+        end if;
+        
+        if std_match(IR, OpCPI   ) then 
+            ALUStatusMask <= flag_mask_ZCNVSH;
+            ALUOp2Sel<= ImmedOp2;
+        end if;
+        
+        if std_match(IR, OpDEC   ) then 
+            ALUStatusMask <= flag_mask_ZNVS;
+        end if;
+        
+        if std_match(IR, OpEOR   ) then 
+            ALUStatusMask <= flag_mask_ZNVS;
+        end if;
+        
+        if std_match(IR, OpINC   ) then 
+            ALUStatusMask <= flag_mask_ZNVS;
+        end if;
+        
+        if std_match(IR, OpLSR   ) then 
+            ALUStatusMask <= flag_mask_ZCNVS;
+        end if;
+        
+        if std_match(IR, OpMUL   ) then 
+            ALUStatusMask <= flag_mask_ZC;
+        end if;
+        
+        if std_match(IR, OpNEG   ) then 
+            ALUStatusMask <= flag_mask_ZCNVSH;
+        end if;
+        
+        if std_match(IR, OpOR    ) then 
+            ALUStatusMask <= flag_mask_ZNVS;
+        end if;
+        
+        if std_match(IR, OpORI   ) then 
+            ALUStatusMask <= flag_mask_ZNVS;
+            ALUOp2Sel<= ImmedOp2;
+        end if;
+        
+        if std_match(IR, OpROR   ) then 
+            ALUStatusMask <= flag_mask_ZCNVS;
+        end if;
+        
+        if std_match(IR, OpSBC   ) then 
+            ALUStatusMask <= flag_mask_ZCNVSH;
+        end if;
+        
+        if std_match(IR, OpSBCI  ) then
+            ALUStatusMask <= flag_mask_ZCNVSH;
+            ALUOp2Sel<= ImmedOp2;        
+        end if;
+        
+        if std_match(IR, OpSBIW  ) then 
+            ALUStatusMask <= flag_mask_ZCNVS;
+            ALUOp2Sel<= ImmedOp2;
+        end if;
+        
+        if std_match(IR, OpSUB   ) then 
+            ALUStatusMask <= flag_mask_ZCNVSH;
+        end if;
+        
+        if std_match(IR, OpSUBI  ) then 
+            ALUStatusMask <= flag_mask_ZCNVSH;
+            ALUOp2Sel<= ImmedOp2;
+        end if;
+        
+        if std_match(IR, OpSWAP  ) then 
+            ALUStatusMask <= flag_mask_KEEPALL;
+        end if;
+    
+    end process DecodeInstruction;
+    
+    
+    UpdateCycleCount: process(clock)
+    begin
+        if rising_edge(clock) then
+            CycleCount <= '0';
+            if CycleCount = '0' and ( std_match(IR, OpMUL) or std_match(IR, OpADIW) or std_match(IR, OpSBIW) ) then
+                CycleCount <= '1';
+            end if;
+        end if;
+    end process UpdateCycleCount;
+    
 end DataFlow;
-
