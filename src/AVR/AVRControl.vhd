a47a124ee3f00f3ef537fda9df8cceefd9435e8a
Bryan He <bryanhe@caltech.edu>
2015-01-29 00:21:31 -0800

    Blank version of registers and control

---
+++ src/AVR/AVRControl.vhd
@@ -0,0 +1,21 @@
+-- bring in necessary libraries
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library opcodes;
+use opcodes.opcodes.all;
+
+library ALUCommands;
+use ALUCommands.ALUCommands.all;
+
+entity AVRControl is
+    port (
+        );
+end AVRControl;
+
+architecture DataFlow of AVRControl is
+begin
+end DataFlow;
+
+
---
+++ src/AVR/AVRRegisters.vhd
@@ -0,0 +1,21 @@
+-- bring in necessary libraries
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library opcodes;
+use opcodes.opcodes.all;
+
+library ALUCommands;
+use ALUCommands.ALUCommands.all;
+
+entity AVRRegisters is
+    port (
+        );
+end AVRRegisters;
+
+architecture DataFlow of AVRRegisters is
+begin
+end DataFlow;
+
+
