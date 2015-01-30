3f021b1e6df10080b04a56e6a7a2e0e186261b61
Albert Gural <ag@albertgural.com>
2015-01-30 10:42:18 -0800

    Re-added SBCI and SUBI to ALU test bench.

--- src/AVR/isim/testbench_isim_beh.exe.sim/work/testbench_isim_beh.exe_main.c
+++ src/AVR/isim/testbench_isim_beh.exe.sim/work/testbench_isim_beh.exe_main.c
@@ -14,14 +14,14 @@
 
 struct XSI_INFO xsi_info;
 
-char *IEEE_P_1242562249;
-char *IEEE_P_3499444699;
-char *STD_STANDARD;
-char *IEEE_P_3620187407;
 char *IEEE_P_2592010699;
 char *OPCODES_P_2431924499;
-char *IEEE_P_3972351953;
+char *STD_STANDARD;
+char *IEEE_P_1242562249;
 char *ALUCOMMANDS_P_1015570589;
+char *IEEE_P_3620187407;
+char *IEEE_P_3499444699;
+char *IEEE_P_3972351953;
 
 
 int main(int argc, char **argv)
@@ -50,15 +50,15 @@ int main(int argc, char **argv)
 
     xsi_register_tops("work_a_1949178628_2494385415");
 
-    IEEE_P_1242562249 = xsi_get_engine_memory("ieee_p_1242562249");
-    IEEE_P_3499444699 = xsi_get_engine_memory("ieee_p_3499444699");
-    STD_STANDARD = xsi_get_engine_memory("std_standard");
-    IEEE_P_3620187407 = xsi_get_engine_memory("ieee_p_3620187407");
     IEEE_P_2592010699 = xsi_get_engine_memory("ieee_p_2592010699");
     xsi_register_ieee_std_logic_1164(IEEE_P_2592010699);
     OPCODES_P_2431924499 = xsi_get_engine_memory("opcodes_p_2431924499");
-    IEEE_P_3972351953 = xsi_get_engine_memory("ieee_p_3972351953");
+    STD_STANDARD = xsi_get_engine_memory("std_standard");
+    IEEE_P_1242562249 = xsi_get_engine_memory("ieee_p_1242562249");
     ALUCOMMANDS_P_1015570589 = xsi_get_engine_memory("alucommands_p_1015570589");
+    IEEE_P_3620187407 = xsi_get_engine_memory("ieee_p_3620187407");
+    IEEE_P_3499444699 = xsi_get_engine_memory("ieee_p_3499444699");
+    IEEE_P_3972351953 = xsi_get_engine_memory("ieee_p_3972351953");
 
     return xsi_run_simulation(argc, argv);
 
