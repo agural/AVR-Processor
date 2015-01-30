f920f1d6642f901408aeac6a339a54a3672cae9b
Bryan He <bryanhe@caltech.edu>
2015-01-29 19:55:40 -0800

    Revert "Pretending to resolve conflicts"
    This reverts commit 9f7503d47ef09ba7fe07ea474eb13470f6dc6e67.

--- src/AVR/fuseRelaunch.cmd
+++ src/AVR/fuseRelaunch.cmd
@@ -1,1 +1,1 @@
--intstyle "ise" -incremental -o "/home/bryanhe/AVR-Processor/src/AVR/ALU_TEST_isim_beh.exe" -prj "/home/bryanhe/AVR-Processor/src/AVR/ALU_TEST_beh.prj" "work.ALU_TEST" 
+-intstyle "ise" -incremental -o "D:/Google Drive/Dropbox/git/AVR-Processor/src/AVR/REG_TEST_TB_isim_beh.exe" -prj "D:/Google Drive/Dropbox/git/AVR-Processor/src/AVR/REG_TEST_TB_beh.prj" "work.REG_TEST_TB" 
