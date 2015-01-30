a0e1bf275d7a8c5d19c64d5bea75bede90ffce28
Bryan He <bryanhe@caltech.edu>
2015-01-29 22:00:10 -0800

    XOR test completed

--- src/AVR/ALU_TEST_TB.vhd
+++ src/AVR/ALU_TEST_TB.vhd
@@ -35,24 +35,54 @@ begin
 
     -- generate the stimulus and test the design
     tb : process
-	     variable answer   : std_logic_vector(7 downto 0);
+       variable answer   : std_logic_vector(7 downto 0);
+       variable status   : std_logic_vector(7 downto 0);
+       constant max_value : integer := ((2 ** 8) - 1); -- maximum possible input
     begin
-        --    "001001rdddddrrrr";
-        IR <= "001001XXXXXXXXXX";
-        OperandA <= "00000001";
-        OperandB <= "00000000";
-        wait for 100 ns;
-		  answer := (OperandA xor OperandB);
+        status := (others => '-');
+        for i in 0 to max_value loop
+            for j in 0 to max_value loop
+                --    "001001rdddddrrrr";
+                IR <= "001001XXXXXXXXXX";
+                OperandA <= std_logic_vector(to_unsigned(i, 8));
+                OperandB <= std_logic_vector(to_unsigned(j, 8));
+                wait until (clock = '1');
+                wait until (clock = '1');
+                answer := (OperandA xor OperandB);
+                
+					 if (answer = "00000000") then
+					     status(flag_Z) := '1';
+					 else
+					     status(flag_Z) := '0';
+				    end if;
 
-        -- verify that result matches
-        assert (std_match(Result, answer))
-            report "Wrong answer for XOR(" &
-                integer'image(to_integer(unsigned(OperandA))) & ", " &
-                integer'image(to_integer(unsigned(OperandB))) & ") = " &
-					 integer'image(to_integer(unsigned(answer))) &
-                " (Got " & integer'image(to_integer(unsigned(Result))) & ")"
-        severity ERROR;
+                status(flag_N) := answer(7);
+					 status(flag_V) := '0';
+					 status(flag_S) := status(flag_N) xor status(flag_V);
+					 
+                -- verify that result matches
+                assert (std_match(Result, answer))
+                    report "Wrong answer for XOR(" &
+                        integer'image(to_integer(unsigned(OperandA))) & ", " &
+                        integer'image(to_integer(unsigned(OperandB))) & ") = " &
+                        integer'image(to_integer(unsigned(answer))) &
+                        " (Got " & integer'image(to_integer(unsigned(Result))) & ")"
+                severity ERROR;
 
+                -- verify that result matches
+                assert (std_match(StatReg, status))
+                    report "Wrong status for XOR(" &
+                        integer'image(to_integer(unsigned(OperandA))) & ", " &
+                        integer'image(to_integer(unsigned(OperandB))) & ") = " &
+                        integer'image(to_integer(unsigned(answer))) & " (Got " &
+								integer'image(to_integer(unsigned(StatReg))) & " instead of " &
+								integer'image(to_integer(unsigned(status))) & ")"
+                severity ERROR;
+            end loop;
+        end loop;
+
+        report "DONE WITH SIMULATIONS";
+        end_sim <= true; -- done with simulations
         wait; -- will wait forever
     end process tb;
 
