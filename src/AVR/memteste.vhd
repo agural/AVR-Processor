210a2fdceb213746e43200f20847b44178e572d1
Bryan He <bryanhe@caltech.edu>
2015-02-05 22:50:26 -0800

    Add memory test entity

---
+++ src/AVR/memteste.vhd
@@ -0,0 +1,61 @@
+----------------------------------------------------------------------------
+--
+--  Atmel AVR Data Memory Test Entity Declaration
+--
+--  This is the entity declaration which must be used for building the data
+--  memory access portion of the AVR design for testing.
+--
+--  Revision History:
+--     24 Apr 98  Glen George       Initial revision.
+--     25 Apr 00  Glen George       Fixed entity name and updated comments.
+--      2 May 02  Glen George       Updated comments.
+--      3 May 02  Glen George       Fixed Reset signal type.
+--     23 Jan 06  Glen George       Updated comments.
+--     21 Jan 08  Glen George       Updated comments.
+--
+----------------------------------------------------------------------------
+
+
+--
+--  MEM_TEST
+--
+--  This is the data memory access testing interface.  It just brings all
+--  the important data memory access signals out for testing along with the
+--  Instruction Register and Program Data Bus.
+--
+--  Inputs:
+--    IR     - Instruction Register (16 bits)
+--    ProgDB - program memory data bus (16 bits)
+--    Reset  - active low reset signal
+--    clock  - the system clock
+--
+--  Outputs:
+--    DataAB - data memory address bus (16 bits)
+--    DataDB - data memory data bus (8 bits)
+--    DataRd - data read (active low)
+--    DataWr - data write (active low)
+--
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.numeric_std.all;
+
+library opcodes;
+use opcodes.opcodes.all;
+
+
+entity  MEM_TEST  is
+
+    port (
+        IR      :  in     opcode_word;                      -- Instruction Register
+        ProgDB  :  in     std_logic_vector(15 downto 0);    -- second word of instruction
+        Reset   :  in     std_logic;                        -- system reset signal (active low)
+        clock   :  in     std_logic;                        -- system clock
+        DataAB  :  out    std_logic_vector(15 downto 0);    -- data address bus
+        DataDB  :  inout  std_logic_vector(7 downto 0);     -- data data bus
+        DataRd  :  out    std_logic;                        -- data read (active low)
+        DataWr  :  out    std_logic                         -- data write (active low)
+    );
+
+end  MEM_TEST;
