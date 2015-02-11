1f5293f6f7e894760d26e6fc6e10b912d0fc3a7a
Bryan He <bryanhe@caltech.edu>
2015-02-10 16:59:38 -0800

    Procedure for ldx and stx

--- src/AVR/MEM_TEST_TB.vhd
+++ src/AVR/MEM_TEST_TB.vhd
@@ -2,6 +2,7 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;       --contains conversion functions
+use ieee.std_logic_unsigned.all;
 
 library opcodes;
 use opcodes.opcodes.all;
@@ -21,6 +22,11 @@ architecture TB_ARCHITECTURE of MEM_TEST_TB is
     signal DataDB  :  std_logic_vector(7 downto 0);     -- data data bus
     signal DataRd  :  std_logic;                        -- data read (active low)
     signal DataWr  :  std_logic;                        -- data write (active low)
+
+    -- define the registers
+    constant NUM_REGS : integer := 96; -- number of registers (including IO)
+    type REG_ARRAY is array (0 to NUM_REGS-1) of std_logic_vector(7 downto 0);
+    signal Registers : REG_ARRAY;
 
     signal end_sim :  boolean := false;                 -- end simulation flag
 begin
@@ -42,31 +48,103 @@ begin
             d : std_logic_vector(3 downto 0);
             k : std_logic_vector(7 downto 0)) is
         begin
-            wait until (clock = '1');
                 -- 1110kkkkddddkkkk
             IR <= "1110XXXXXXXXXXXX";
             IR(11 downto 8) <= k(7 downto 4);
             IR( 3 downto 0) <= k(3 downto 0);
             IR( 7 downto 4) <= d;
+            Registers(conv_integer('1' & d)) <= k;
+            wait until (clock = '1');
         end procedure;
 
         procedure run_LDX (
-            d : std_logic_vector(4 downto 0)) is
+            d : std_logic_vector(4 downto 0);
+            k : std_logic_vector(7 downto 0)) is
+            variable address : std_logic_vector(15 downto 0);
         begin
-            wait until (clock = '1');
+            address := (Registers(27) & Registers(26));
                 -- 1001000ddddd1100
             IR <= "1001000XXXXX1100";
             IR(8 downto 4) <= d;
+            wait until (clock = '0');
+            wait for 1 ns;
+            assert (DataRd = '1');
+            assert (DataWr = '1');
+
             wait until (clock = '1');
+            wait for 1 ns;
+            assert (DataRd = '1');
+            assert (DataWr = '1');
+            if (conv_integer(address) > 95) then
+                assert (DataAB = address);
+            end if;
+
+            wait until (clock = '0');
+            wait for 1 ns;
+            assert (DataWr = '1');
+            if (conv_integer(address) > 95) then
+                Registers(conv_integer(d)) <= k;
+                assert (DataRd = '0');
+                assert (DataAB = (Registers(27) & Registers(26)));
+                DataDB <= k;
+            else
+                Registers(conv_integer(d)) <= Registers(conv_integer(address));
+                assert (DataRd = '1');
+            end if;
+
+            wait until (clock = '1');
+            wait for 1 ns;
+            assert (DataRd = '1');
+            assert (DataWr = '1');
+
+        end procedure;
+
+        procedure run_STX (
+            d : std_logic_vector(4 downto 0)) is
+            variable address : std_logic_vector(15 downto 0);
+        begin
+            address := (Registers(27) & Registers(26));
+                -- 1001000ddddd1100
+            IR <= "1001000XXXXX1100";
+            IR(8 downto 4) <= d;
+            wait until (clock = '0');
+            wait for 1 ns;
+            assert (DataRd = '1');
+            assert (DataWr = '1');
+
+            wait until (clock = '1');
+            wait for 1 ns;
+            assert (DataRd = '1');
+            assert (DataWr = '1');
+            if (conv_integer(address) > 95) then
+                assert (DataAB = address);
+            end if;
+
+            wait until (clock = '0');
+            wait for 1 ns;
+            assert (DataRd = '1');
+            if (conv_integer(address) > 95) then
+                assert (DataWr = '0');
+                assert (DataAB = (Registers(27) & Registers(26)));
+                assert (DataDB = Registers(conv_integer(d)));
+            else
+                Registers(conv_integer(address)) <= Registers(conv_integer(d));
+                assert (DataWr = '1');
+            end if;
+
+            wait until (clock = '1');
+            wait for 1 ns;
+            assert (DataRd = '1');
+            assert (DataWr = '1');
         end procedure;
     begin
         wait for 25 ns;
 
-        run_LDI("0000", "00000000");
-        for i in 0 to 16 loop
+        wait until (clock = '1');
+        for i in 0 to 15 loop
             run_LDI(std_logic_vector(to_unsigned(i, 4)), "0000" & std_logic_vector(to_unsigned(i, 4)));
         end loop;
-        run_LDX("00000");
+        run_LDX("00000", "00000000");
 
         wait until (clock = '1');
         wait until (clock = '1');
